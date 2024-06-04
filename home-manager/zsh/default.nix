e3015eda90a611cf8a606f083e56730e8924ce1f
Caleb Norton <n0603919@outlook.com>
2024-06-04 10:45:33 -0500

    Customize zsh config

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -1,18 +1,48 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     meslo-lgs-nf
+    fzf
   ];
+
   programs.zsh = {
     enable = true;
-    initExtra = ''
-      [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh}
+    syntaxHighlighting = {
+      enable = true;
+    };
+    autosuggestion = {
+      enable = true;
+    };
+    historySubstringSearch = {
+      enable = true;
+    };
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
     '';
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
+    initExtra = ''
+      # Plugins
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh}
+
+      # Completion styling
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+
+      # Shell integrations
+      eval "$(fzf --zsh)"
+    '';
+    shellAliases = {
+      ls = "ls --color=auto";
+      ll = "ls -l";
+      la = "ls -la";
+      l = "ls";
+    };
   };
 }
