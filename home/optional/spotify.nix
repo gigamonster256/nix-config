2ba4a128bd4b059d02c53baaeb25816ea6d94e7d
Caleb Norton <n0603919@outlook.com>
2024-06-24 14:17:52 -0500

    Switch to Dribbblish theme

--- home/optional/spotify.nix
+++ home/optional/spotify.nix
@@ -5,19 +5,16 @@
 }: let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in {
-  # import the flake's module for your system
   imports = [inputs.spicetify-nix.homeManagerModules.default];
 
-  # configure spicetify :)
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+    theme = spicePkgs.themes.dribbblish;
+    colorScheme = "catppuccin-mocha";
 
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
-      shuffle # shuffle+ (special characters are sanitized out of ext names)
-      # hidePodcasts
+      shuffle
     ];
   };
 }
