804cbb069b743c977b9ba1c91ad2cd9a296464a5
Caleb Norton <n0603919@outlook.com>
2024-06-29 17:13:49 -0500

    Cleanup pkgs

--- pkgs/sketchybar-plugins/default.nix
+++ pkgs/sketchybar-plugins/default.nix
@@ -1,9 +1,7 @@
-{pkgs}: rec {
+{pkgs}: let
+  attrsToPaths = attrs: pkgs.lib.mapAttrsToList (name: path: {inherit name path;}) attrs;
+  allPlugins = pkgs.lib.filterAttrs (n: v: n != "all") pkgs.sketchybar-plugins;
+in {
   builtin = pkgs.callPackage ./builtin.nix {};
-  all = pkgs.symlinkJoin {
-    name = "sketchybar-plugins";
-    paths = [
-      builtin
-    ];
-  };
+  all = pkgs.linkFarm "sketchybar-plugins" (attrsToPaths allPlugins);
 }
--- pkgs/waybar-themes/default.nix
+++ pkgs/waybar-themes/default.nix
@@ -1,3 +1,7 @@
-{pkgs}: {
+{pkgs}: let
+  attrsToPaths = attrs: pkgs.lib.mapAttrsToList (name: path: {inherit name path;}) attrs;
+  allThemes = pkgs.lib.filterAttrs (n: v: n != "all") pkgs.waybar-themes;
+in {
   catppuccin = pkgs.callPackage ./catppuccin.nix {};
+  all = pkgs.linkFarm "waybar-themes" (attrsToPaths allThemes);
 }
