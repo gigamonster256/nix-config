92fdaa808fcb9c971185087c0c440f4262332526
Caleb Norton <n0603919@outlook.com>
2024-06-30 21:20:01 -0500

    Revert some btop changes

--- home/optional/btop.nix
+++ home/optional/btop.nix
@@ -1,13 +1,9 @@
-{pkgs, ...}: let
-  btop-with-themes = pkgs.symlinkJoin {
-    name = "btop-with-themes";
-    paths = with pkgs; [
-      btop
-      btop-themes.catppuccin
-    ];
-  };
-in {
-  home.packages = [
-    btop-with-themes
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    btop
   ];
+
+  home.file."./.config/btop/themes" = {
+    source = "${pkgs.btop-themes.catppuccin}/share/btop/themes";
+  };
 }
