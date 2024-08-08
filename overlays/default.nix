68f6022ab60cfb800e3d9bca4dacece6b80c19af
Caleb Norton <n0603919@outlook.com>
2024-08-08 12:34:28 -0500

    Remove darwin man-db override

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,33 +15,6 @@
       version = "0.63.6";
       src = ../trilium-mac-arm64-0.63.6.zip;
     });
-    # disable systemd on darwin for man-db
-    man = prev.man.overrideAttrs (oldAttrs: {
-      configureFlags = let
-        systemdtmpfilesdir =
-          if final.stdenv.hostPlatform.isDarwin
-          then "no"
-          else "${placeholder "out"}/lib/tmpfiles.d";
-        systemdsystemunitdir =
-          if final.stdenv.hostPlatform.isDarwin
-          then "no"
-          else "${placeholder "out"}/lib/systemd/system";
-      in
-        [
-          "--disable-setuid"
-          "--disable-cache-owner"
-          "--localstatedir=/var"
-          "--with-config-file=${placeholder "out"}/etc/man_db.conf"
-          "--with-systemdtmpfilesdir=${systemdtmpfilesdir}"
-          "--with-systemdsystemunitdir=${systemdsystemunitdir}"
-          "--with-pager=less"
-        ]
-        ++ final.lib.optionals final.stdenv.hostPlatform.isDarwin [
-          "ac_cv_func__set_invalid_parameter_handler=no"
-          "ac_cv_func_posix_fadvise=no"
-          "ac_cv_func_mempcpy=no"
-        ];
-    });
 
     # something funky is going on with gitstatus
     gitstatus = prev.gitstatus.overrideAttrs (oldAttrs: {
