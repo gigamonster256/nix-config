355ac10acb57fed9e52e9ed5062467aeb3165dc4
Caleb Norton <n0603919@outlook.com>
2024-11-20 12:48:15 -0600

    Remove sops-nix input following

--- flake.nix
+++ flake.nix
@@ -18,7 +18,6 @@
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
     };
     nh_plus = {
       url = "github:ToyVo/nh_plus";
