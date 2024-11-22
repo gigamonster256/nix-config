f413938e3b21c753ab602656b4610ab839fe9cf5
Caleb Norton <n0603919@outlook.com>
2024-11-22 14:54:14 -0600

    Dont export packages

--- flake.nix
+++ flake.nix
@@ -58,7 +58,6 @@
         ...
       }: {
         formatter = pkgs.alejandra;
-        packages = import ./pkgs {inherit pkgs;};
 
         pre-commit.settings.hooks.alejandra.enable = true;
         devShells.default = config.pre-commit.devShell;
