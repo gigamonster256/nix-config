6caea3d29a53ea779bd46aee85a6c198ef4d0334
Caleb Norton <n0603919@outlook.com>
2025-01-08 00:03:11 -0600

    ghostty: add darwin support

--- flake.nix
+++ flake.nix
@@ -14,6 +14,10 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    gigamonster256-nur = {
+      url = "github:gigamonster256/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     lite-config.url = "github:gigamonster256/lite-config";
     sops-nix = {
       url = "github:Mic92/sops-nix";
--- home/global/ghostty.nix
+++ home/global/ghostty.nix
@@ -1,17 +1,25 @@
 {
+  inputs,
   pkgs,
   lib,
   config,
   ...
-}: let
-  window-style =
-    if pkgs.stdenv.hostPlatform.isLinux
-    then "window-decoration = false"
-    else "macos-titlebar-style = hidden";
-in {
-  home.packages = lib.optional pkgs.stdenv.hostPlatform.isLinux pkgs.unstable.ghostty;
+}: {
+  home.packages = let
+    ghostty-pkg =
+      if pkgs.stdenv.hostPlatform.isLinux
+      then pkgs.unstable.ghostty
+      else inputs.gigamonster256-nur.packages.${pkgs.stdenv.hostPlatform.system}.ghostty-darwin;
+  in [
+    ghostty-pkg
+  ];
 
-  home.file."${config.xdg.configHome}/ghostty/config" = {
+  home.file."${config.xdg.configHome}/ghostty/config" = let
+    window-style =
+      if pkgs.stdenv.hostPlatform.isLinux
+      then "window-decoration = false"
+      else "macos-titlebar-style = hidden";
+  in {
     text = ''
       # use zsh on path (for nix)
       command = zsh
