c21d3db034de88a73632d366d9054de4df855ca9
Caleb Norton <n0603919@outlook.com>
2025-03-11 16:12:18 -0500

    clean up overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,21 +1,23 @@
 # This file defines overlays
-{inputs, ...}: {
+{inputs, ...}: let
+  customPkgs = pkgs: import ../pkgs {inherit pkgs;};
+in {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: _prev: customPkgs final;
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
+  modifications = final: _prev: {
     # custom trilium-next-desktop package
-    trilium-desktop = final.trilium-next-desktop;
+    trilium-desktop = (customPkgs final).trilium-next-desktop;
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      inherit (final) system;
       config.allowUnfree = true;
     };
   };
