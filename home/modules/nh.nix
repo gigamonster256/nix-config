dad7010d230b3c789a3fa4279e0d1675496af709
Caleb Norton <n0603919@outlook.com>
2025-03-11 13:15:48 -0500

    install nh beta

--- home/modules/nh.nix
+++ home/modules/nh.nix
@@ -2,15 +2,9 @@
   inputs,
   pkgs,
   ...
-}: let
-  inherit (pkgs.stdenv) isDarwin;
-  nh-pkg =
-    if isDarwin
-    then inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default
-    else pkgs.nh;
-in {
+}: {
   home.packages = [
-    nh-pkg
+    inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
   home.sessionVariables.NH_FLAKE = "github:gigamonster256/nix-config";
 }
