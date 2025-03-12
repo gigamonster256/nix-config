5d2d630970edde3364da3a5fd73d42b8e479d1f5
Caleb Norton <n0603919@outlook.com>
2025-03-12 18:27:13 -0500

    add useful compression helpers

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -41,6 +41,40 @@
         --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
         --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
     '';
+    initExtra = ''
+      flash(){
+          if [ $(${pkgs.file}/bin/file $1 --mime-type -b) = "application/zstd" ]; then
+            echo "Flashing zst using zstdcat | dd"
+            ( set -x; ${pkgs.zstd}/bin/zstdcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
+          elif [ $(${pkgs.file}/bin/file $1 --mime-type -b) = "application/x-xz" ]; then
+            echo "Flashing xz using xzcat | dd"
+            ( set -x; ${pkgs.xz}/bin/xzcat $1 | sudo dd of=$2 status=progress iflag=fullblock oflag=direct conv=fsync,noerror bs=64k )
+          else
+            echo "Flashing arbitrary file $1 to $2"
+            ( set -x; sudo dd if=$1 of=$2 status=progress conv=sync,noerror bs=64k )
+          fi
+      }
+      extract(){
+         if [ -f $1 ] ; then
+             case $1 in
+                 *.tar.bz2)   tar xjf $1;;
+                 *.tar.gz)    tar xzf $1;;
+                 *.bz2)       bunzip2 $1;;
+                 *.rar)       rar x $1;;
+                 *.gz)        gunzip $1;;
+                 *.tar)       tar xf $1;;
+                 *.tbz2)      tar xjf $1;;
+                 *.tgz)       tar xzf $1;;
+                 *.zip)       unzip $1;;
+                 *.Z)         uncompress $1;;
+                 *.7z)        7z x $1;;
+                 *)           echo "'$1' cannot be extracted via extract()";;
+             esac
+         else
+             echo "'$1' is not a valid file"
+         fi
+      }
+    '';
     shellAliases = {
       ls = "ls --color=auto";
       ll = "ls -l";
