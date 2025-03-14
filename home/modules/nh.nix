cd6fd1ff01d9b2ad22dee6d4b26197a959120178
Caleb Norton <n0603919@outlook.com>
2025-03-14 17:47:50 -0500

    use nh home-manager module

--- home/modules/nh.nix
+++ home/modules/nh.nix
@@ -3,8 +3,13 @@
   pkgs,
   ...
 }: {
-  home.packages = [
-    inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
+  programs.nh = {
+    enable = true;
+    # use nh beta - adds darwin support and repl helpers
+    package = inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    # TODO: wait for home-manager/#6468 to land in my home-manager channel
+    # flake = "github:gigamonster256/nix-config";
+  };
+  # TODO: the beta changes the session variable name to NH_FLAKE
   home.sessionVariables.NH_FLAKE = "github:gigamonster256/nix-config";
 }
