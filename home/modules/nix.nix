ed4ad73e2322ffdabf2fba2d07ba3e1440582d7d
Caleb Norton <n0603919@outlook.com>
2025-03-20 11:53:50 -0500

    fixup substituters

--- home/modules/nix.nix
+++ home/modules/nix.nix
@@ -14,13 +14,11 @@ in {
         "flakes"
         "ca-derivations"
       ];
-      substituters = [
-        "https://cache.nixos.org"
+      extra-substituters = [
         "https://nix-community.cachix.org"
         "https://gigamonster256.cachix.org"
       ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "gigamonster256.cachix.org-1:ySCUrOkKSOPm+UTipqGtGH63zybcjxr/Wx0UabASvRc="
       ];
--- hosts/modules/nix.nix
+++ hosts/modules/nix.nix
@@ -14,11 +14,11 @@ in {
         "flakes"
         "ca-derivations"
       ];
-      substituters = [
+      extra-substituters = [
         "https://nix-community.cachix.org"
         "https://gigamonster256.cachix.org"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "gigamonster256.cachix.org-1:ySCUrOkKSOPm+UTipqGtGH63zybcjxr/Wx0UabASvRc="
       ];
