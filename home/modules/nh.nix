08d5ba93ed3fb5c9a1460083929d8e5a3748493b
Caleb Norton <n0603919@outlook.com>
2025-03-25 12:25:43 -0500

    alias nh os on darwin

--- home/modules/nh.nix
+++ home/modules/nh.nix
@@ -1,12 +1,31 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
-  programs.nh = {
+  programs.nh = let
+    inherit (pkgs) stdenv writeShellApplication;
+    # wrap nh in a function that turns "nh os" into "nh darwin"
+    package =
+      if stdenv.hostPlatform.isDarwin
+      then
+        writeShellApplication {
+          name = "nh";
+          runtimeInputs = [inputs.nh.packages.${stdenv.hostPlatform.system}.default];
+          text = ''
+            if [[ "$1" == "os" ]]; then
+              shift
+              set -- darwin "$@"
+            fi
+            nh "$@"
+          '';
+        }
+      else inputs.nh.packages.${stdenv.hostPlatform.system}.default;
+  in {
     enable = true;
     # use nh beta - adds darwin support and repl helpers
-    package = inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    inherit package;
     # TODO: wait for home-manager/#6468 to land in my home-manager channel
     # flake = "github:gigamonster256/nix-config";
   };
