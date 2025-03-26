831b14bdedc6ec380d59883c3d83386f6457f9cd
Caleb Norton <n0603919@outlook.com>
2025-03-26 16:49:16 -0500

    don't wrap nh on darwin

--- home/modules/nh.nix
+++ home/modules/nh.nix
@@ -4,28 +4,10 @@
   lib,
   ...
 }: {
-  programs.nh = let
-    inherit (pkgs) stdenv writeShellApplication;
-    # wrap nh in a function that turns "nh os" into "nh darwin"
-    package =
-      if stdenv.hostPlatform.isDarwin
-      then
-        writeShellApplication {
-          name = "nh";
-          runtimeInputs = [inputs.nh.packages.${stdenv.hostPlatform.system}.default];
-          text = ''
-            if [[ "$1" == "os" ]]; then
-              shift
-              set -- darwin "$@"
-            fi
-            nh "$@"
-          '';
-        }
-      else inputs.nh.packages.${stdenv.hostPlatform.system}.default;
-  in {
+  programs.nh = {
     enable = true;
     # use nh beta - adds darwin support and repl helpers
-    inherit package;
+    package = inputs.nh.packages.${pkgs.stdenv.hostPlatform.system}.default;
     # TODO: wait for home-manager/#6468 to land in my home-manager channel
     # flake = "github:gigamonster256/nix-config";
   };
