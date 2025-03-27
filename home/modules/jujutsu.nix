323620d79fffc1997572314c6dbb85557e112d8b
Caleb Norton <n0603919@outlook.com>
2025-03-27 11:57:19 -0500

    use latest jj and set merge editor

--- home/modules/jujutsu.nix
+++ home/modules/jujutsu.nix
@@ -1,10 +1,12 @@
-{config, ...}: {
+{pkgs,config, ...}: {
   programs.jujutsu = {
     enable = true;
+    package = pkgs.unstable.jujutsu;
     settings = {
       user = {
         name = config.programs.git.userName;
         email = config.programs.git.userEmail;
+        ui.merge-editor = ":builtin";
       };
     };
   };
