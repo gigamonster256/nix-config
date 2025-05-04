10cb551963b1a61a49ac6296a94464f6276ef9eb
Caleb Norton <n0603919@outlook.com>
2025-05-04 12:01:12 -0500

    more impermanence prep

--- hosts/littleboy/disko.nix
+++ hosts/littleboy/disko.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   disko.devices.disk.main = {
     type = "disk";
     device = "/dev/disk/by-id/mmc-TA2964_0x5a021577";
@@ -35,13 +35,6 @@
                     "noatime"
                   ];
                 };
-                # "home" = {
-                #   mountpoint = "/home";
-                #   mountOptions = [
-                #     "compress=zstd"
-                #     "noatime"
-                #   ];
-                # };
                 "nix" = {
                   mountpoint = "/nix";
                   mountOptions = [
@@ -62,5 +55,45 @@
         };
       };
     };
+  };
+
+  # impermanence
+  fileSystems."/persist".neededForBoot = true;
+  boot.initrd.systemd.extraBin = {
+    mkdir = "${pkgs.coreutils}/bin/mkdir";
+  };
+  boot.initrd.systemd.services.btrfs-prepare = {
+    description = "Prepare btrfs subvolumes for root";
+    wantedBy = ["initrd.target"];
+    after = ["dev-mapper-crypted.device"];
+    before = ["sysroot.mount"];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir /btrfs_tmp
+      mount /dev/mapper/crypted /btrfs_tmp
+      if [[ -e /btrfs_tmp/root ]]; then
+          mkdir -p /btrfs_tmp/old_roots
+          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+      fi
+
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for subvol in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$subvol"
+          done
+          btrfs subvolume delete "$1"
+      }
+
+      # Delete old roots after 30 days
+      for old_root in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+          delete_subvolume_recursively "$old_root"
+      done
+
+      # Create new root subvolume
+      btrfs subvolume create /btrfs_tmp/root
+      umount /btrfs_tmp
+    '';
   };
 }
