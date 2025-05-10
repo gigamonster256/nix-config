78944434e23ae3ca27b7093e6cc30e2cfddbf5cf
Caleb Norton <n0603919@outlook.com>
2025-05-10 12:58:33 -0500

    better font dependencies

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -4,37 +4,46 @@
   lib,
   config,
   ...
-}: {
-  programs.ghostty = {
-    # TODO: refactor when ghostty makes it into nixpkgs for darwin
-    package =
-      if pkgs.stdenv.hostPlatform.isLinux
-      then pkgs.unstable.ghostty
-      else inputs.gigamonster256-nur.packages.${pkgs.stdenv.hostPlatform.system}.ghostty-darwin;
-    settings = let
-      # Monaspace Neon
-      font = "Monaspace Neon Var";
-      # Monaspace Radon (cursiveish)
-      italic-font = "Monaspace Radon Var";
-    in
-      lib.mkDefault {
-        command = "${lib.getExe config.programs.zsh.package}";
-        theme = "catppuccin-mocha";
-        background-opacity = 0.85;
-        mouse-hide-while-typing = true;
-        focus-follows-mouse = true;
-        window-decoration = false;
-        macos-titlebar-style = "hidden";
-        font-family = font;
-        font-family-italic = italic-font;
-        font-family-bold-italic = italic-font;
-        config-file = "?nix-escape-hatch";
-        # https://github.com/githubnext/monaspace?tab=readme-ov-file#character-variants
-        font-feature = [
-          "cv01=2" # slashed 0s
-          "+cv02" # no bottom serif on 1
-          "+cv31" # 6 pointed asterisk
-        ];
-      };
-  };
-}
+}:
+lib.mkMerge [
+  {
+    programs.ghostty = {
+      # TODO: refactor when ghostty makes it into nixpkgs for darwin
+      package =
+        if pkgs.stdenv.hostPlatform.isLinux
+        then pkgs.unstable.ghostty
+        else inputs.gigamonster256-nur.packages.${pkgs.stdenv.hostPlatform.system}.ghostty-darwin;
+      settings = let
+        # Monaspace Neon
+        font = "Monaspace Neon Var";
+        # Monaspace Radon (cursiveish)
+        italic-font = "Monaspace Radon Var";
+      in
+        lib.mkDefault {
+          command = "${lib.getExe config.programs.zsh.package}";
+          theme = "catppuccin-mocha";
+          background-opacity = 0.85;
+          mouse-hide-while-typing = true;
+          focus-follows-mouse = true;
+          window-decoration = false;
+          macos-titlebar-style = "hidden";
+          font-family = font;
+          font-family-italic = italic-font;
+          font-family-bold-italic = italic-font;
+          config-file = "?nix-escape-hatch";
+          # https://github.com/githubnext/monaspace?tab=readme-ov-file#character-variants
+          font-feature = [
+            "cv01=2" # slashed 0s
+            "+cv02" # no bottom serif on 1
+            "+cv31" # 6 pointed asterisk
+          ];
+        };
+    };
+  }
+  (lib.mkIf config.programs.ghostty.enable {
+    fonts.fontconfig.enable = lib.mkDefault true;
+    home.packages = [
+      pkgs.unstable.monaspace
+    ];
+  })
+]
--- hosts/modules/default.nix
+++ hosts/modules/default.nix
@@ -1,14 +1,3 @@
 {
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
   imports = [./nix.nix];
-
-  fonts.packages = [
-    (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
-    # monaspace 1.200 has nerd fonts built in
-    pkgs.unstable.monaspace
-  ];
 }
