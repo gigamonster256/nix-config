46de48820d8233b6973b5aca8ec25f01f47a8780
Caleb Norton <n0603919@outlook.com>
2025-05-26 12:20:16 -0500

    gh setup

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,6 +1,12 @@
-{ lib, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 let
   inherit (lib) mkDefault;
+  gitcfg = config.programs.git;
+  ghcfg = config.programs.gh;
 in
 {
   programs.git = {
@@ -8,6 +14,18 @@ in
     userEmail = mkDefault "n0603919@outlook.com";
     aliases = {
       exec = mkDefault "!exec ";
+    };
+  };
+  programs.gh = {
+    enable = mkDefault gitcfg.enable;
+    settings = {
+      git_protocol = mkDefault "ssh";
+    };
+    hosts = {
+      "github.com" = {
+        git_protocol = mkDefault ghcfg.settings.git_protocol;
+        user = mkDefault "gigamonster256";
+      };
     };
   };
 }
