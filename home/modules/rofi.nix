858ed10a50889e0eef44a0d0dca412ad408e55bf
Caleb Norton <n0603919@outlook.com>
2025-06-01 17:02:45 -0500

    set rofi terminal

--- home/modules/rofi.nix
+++ home/modules/rofi.nix
@@ -5,12 +5,13 @@
   ...
 }:
 let
-  inherit (lib) mkDefault;
+  inherit (lib) mkDefault getExe;
 in
 {
   programs.rofi = {
     enable = mkDefault config.wayland.windowManager.hyprland.enable;
     package = mkDefault pkgs.rofi-wayland;
     plugins = mkDefault [ pkgs.rofi-emoji-wayland ];
+    terminal = mkDefault "${getExe config.programs.ghostty.package}";
   };
 }
