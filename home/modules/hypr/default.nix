4b06f18e8fbee5bf1f4a173517028bf601a53aef
Caleb Norton <n0603919@outlook.com>
2025-06-08 17:54:33 -0500

    use nautilus as file picker

--- home/modules/hypr/default.nix
+++ home/modules/hypr/default.nix
@@ -1,7 +1,28 @@
 {
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+in
+{
   imports = [
     ./hyprland.nix
     ./hyprlock.nix
     ./hypridle.nix
   ];
+
+  # gtk portal and nautilus as file picker
+  config = mkIf config.wayland.windowManager.hyprland.enable {
+    home.packages = [
+      pkgs.nautilus
+    ];
+    xdg.portal = {
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      xdgOpenUsePortal = true;
+      config.gtk."org.freedesktop.impl.portal.FileChooser" = "nautilus";
+    };
+  };
 }
