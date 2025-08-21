cb93295e94bbabd8b8e97544f90b658bb7f548ab
Caleb Norton <n0603919@outlook.com>
2025-08-21 15:47:29 -0500

    programatically generate vpn polkit rule

--- hosts/modules/nixos/vpn.nix
+++ hosts/modules/nixos/vpn.nix
@@ -38,13 +38,28 @@
     };
   };
 
-  security.polkit.extraConfig = # javascript
+  security.polkit.extraConfig =
+    let
+      # https://github.com/NixOS/nixpkgs/blob/874be15f3c74c7d15cb804e485428fd444f8755f/nixos/modules/services/networking/wg-quick.nix#L370
+      wg-quickServices = lib.mapAttrsToList (
+        name: _: "wg-quick-${name}.service"
+      ) config.networking.wg-quick.interfaces;
+      # https://github.com/NixOS/nixpkgs/blob/2053850561181daa345d5607bc996c945a0ebc06/nixos/modules/services/networking/openconnect.nix#L157
+      openconnectServices = lib.mapAttrsToList (
+        name: _: "openconnect-${name}.service"
+      ) config.networking.openconnect.interfaces;
+      allServices = wg-quickServices ++ openconnectServices;
+      # var == unit1 || var == unit2 || ...
+      equalsAny = var: l: lib.concatMapStringsSep " || " (svc: "${var} == '${svc}'") l;
+    in
+    # TODO: use js list.includes?
+    # javascript
     ''
       polkit.addRule(function(action, subject) {
         if (action.id == "org.freedesktop.systemd1.manage-units" &&
           subject.isInGroup("vpn")) {
           var unit = action.lookup("unit");
-          if (unit == "openconnect-TAMU.service") {
+          if (${equalsAny "unit" allServices}) {
             return polkit.Result.YES;
           }
         }
