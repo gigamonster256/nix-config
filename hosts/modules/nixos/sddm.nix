8e22809942fff9d3138c9a9c46cb1cd0309f950c
Caleb Norton <n0603919@outlook.com>
2025-08-24 14:34:43 -0500

    fix headless installs

--- hosts/modules/nixos/sddm.nix
+++ hosts/modules/nixos/sddm.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkDefault;
+in
 {
   environment.systemPackages = [
     pkgs.catppuccin-sddm
@@ -7,8 +15,8 @@
     # defaultSession = "hyprland-uwsm"; # default is first installed desktop (fine if only 1 installed)
     # FIXME: font hardcode this
     autoLogin = {
-      enable = true;
-      user = "caleb";
+      enable = config.services.displayManager.defaultSession != null; # some window manager is enabled
+      user = mkDefault "caleb";
     };
     sddm = {
       enable = true;
@@ -16,7 +24,7 @@
       theme = "catppuccin-mocha";
       # issue with missing sddm-greeter-qt6
       package = pkgs.kdePackages.sddm;
-      autoLogin.relogin = true;
+      autoLogin.relogin = mkDefault config.services.displayManager.autoLogin.enable;
     };
   };
 }
