04eff33a5d3f924edf2e8e0a07221e45ace6db26
Caleb Norton <n0603919@outlook.com>
2025-09-04 01:01:51 -0500

    switch to nixpkgs-unstable
    nixos-unstable advances without building darwin this can lead to things
    like the darwin stdenv not being cached
    nixpkgs at least makes sure everything is cached
    (but does not run nixos tests)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nix config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     # modular flakes
     flake-parts.url = "github:hercules-ci/flake-parts";
