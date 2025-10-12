cba8b69a9b925c8ed1b30837ee12435b45e34d88
Caleb Norton <n0603919@outlook.com>
2025-10-12 18:57:41 -0500

    convert wrapped apps to flake-module style

--- modules/_impermanence/wrapped-apps.nix
+++ modules/_impermanence/wrapped-apps.nix
@@ -1,4 +1,3 @@
-{ config, ... }:
 let
   mkPersistentProgram =
     {
@@ -8,6 +7,7 @@ let
       packageOptions ? { },
       directories ? [ ],
       files ? [ ],
+      packageSet ? null,
     }:
     {
       lib,
@@ -18,6 +18,7 @@ let
     let
       inherit (lib) mkIf mkEnableOption mkPackageOption;
       cfg = config.programs.${name};
+      pkgs' = if packageSet != null then packageSet else pkgs;
     in
     {
       options = {
@@ -25,7 +26,7 @@ let
           enable = mkEnableOption name // {
             default = defaultEnable;
           };
-          package = mkPackageOption pkgs packageName packageOptions;
+          package = mkPackageOption pkgs' packageName packageOptions;
         };
       };
 
@@ -37,61 +38,68 @@ let
       };
     };
 in
+{ moduleWithSystem, ... }:
 {
-  imports = [
-    (mkPersistentProgram {
-      name = "trilium";
-      packageName = "trilium-next-desktop";
-      directories = [ ".local/share/trilium-data" ];
-    })
-    (mkPersistentProgram {
-      name = "slack";
-      directories = [ ".config/Slack" ];
-    })
-    (mkPersistentProgram {
-      name = "sonusmix";
-      directories = [ ".local/share/org.sonusmix.Sonusmix" ];
-      # defaultEnable = config.wayland.windowManager.hyprland.enable;
-    })
-    (mkPersistentProgram {
-      name = "bitwarden";
-      packageName = "bitwarden-desktop";
-      directories = [ ".config/Bitwarden" ];
-    })
-    (mkPersistentProgram {
-      name = "cemu";
-      directories = [
-        ".config/Cemu"
-        ".local/share/Cemu"
-        ".cache/Cemu"
+  flake.modules.homeManager.base = moduleWithSystem (
+    { self', ... }:
+    {
+      imports = [
+        (mkPersistentProgram {
+          name = "trilium";
+          packageName = "trilium-next-desktop";
+          directories = [ ".local/share/trilium-data" ];
+        })
+        (mkPersistentProgram {
+          name = "slack";
+          directories = [ ".config/Slack" ];
+        })
+        (mkPersistentProgram {
+          name = "sonusmix";
+          directories = [ ".local/share/org.sonusmix.Sonusmix" ];
+          # defaultEnable = config.wayland.windowManager.hyprland.enable;
+        })
+        (mkPersistentProgram {
+          name = "bitwarden";
+          packageName = "bitwarden-desktop";
+          directories = [ ".config/Bitwarden" ];
+        })
+        (mkPersistentProgram {
+          name = "cemu";
+          directories = [
+            ".config/Cemu"
+            ".local/share/Cemu"
+            ".cache/Cemu"
+          ];
+        })
+        (mkPersistentProgram {
+          name = "ryujinx";
+          packageName = "ryubing";
+          directories = [ ".config/Ryujinx" ];
+        })
+        (mkPersistentProgram {
+          name = "wiiu-downloader";
+          directories = [ ".config/WiiUDownloader" ];
+          packageSet = self'.packages; # pull from self packages
+        })
+        (mkPersistentProgram {
+          name = "element";
+          packageName = "element-desktop";
+          directories = [ ".config/Element" ];
+        })
+        (mkPersistentProgram {
+          name = "newsflash";
+          directories = [
+            ".config/news-flash"
+            ".local/share/news-flash"
+          ];
+        })
+        (mkPersistentProgram {
+          name = "prismlauncher";
+          directories = [
+            ".local/share/PrismLauncher"
+          ];
+        })
       ];
-    })
-    (mkPersistentProgram {
-      name = "ryujinx";
-      packageName = "ryubing";
-      directories = [ ".config/Ryujinx" ];
-    })
-    (mkPersistentProgram {
-      name = "wiiu-downloader";
-      directories = [ ".config/WiiUDownloader" ];
-    })
-    (mkPersistentProgram {
-      name = "element";
-      packageName = "element-desktop";
-      directories = [ ".config/Element" ];
-    })
-    (mkPersistentProgram {
-      name = "newsflash";
-      directories = [
-        ".config/news-flash"
-        ".local/share/news-flash"
-      ];
-    })
-    (mkPersistentProgram {
-      name = "prismlauncher";
-      directories = [
-        ".local/share/PrismLauncher"
-      ];
-    })
-  ];
+    }
+  );
 }
