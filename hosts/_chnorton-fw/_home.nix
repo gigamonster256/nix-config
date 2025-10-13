8f956e37ec5d75109ca54cebf47c48d54b24ef9b
Caleb Norton <n0603919@outlook.com>
2025-10-12 19:51:48 -0500

    convert home manager profiles

--- hosts/_chnorton-fw/_home.nix
+++ hosts/_chnorton-fw/_home.nix
@@ -1,53 +1,63 @@
-{ pkgs, ... }:
+{ moduleWithSystem, ... }:
 {
-  home.packages = builtins.attrValues {
-    inherit (pkgs)
-      wpa_supplicant_gui
-      ntop
-      chirp
-      ;
-    inherit (pkgs.kdePackages)
-      okular
-      ;
-    inherit (pkgs.python3Packages)
-      meshtastic
-      ;
+  configurations.nixos.chnorton-fw = {
+    home-manager.users.caleb = moduleWithSystem (
+      { self', ... }:
+      { pkgs, ... }:
+      {
+        home.packages = builtins.attrValues {
+          inherit (pkgs)
+            wpa_supplicant_gui
+            chirp
+            ;
+          inherit (pkgs.kdePackages)
+            okular
+            ;
+          inherit (pkgs.python3Packages)
+            meshtastic
+            ;
+          inherit (self'.packages)
+            ntop
+            ;
+        };
+
+        # amd gpu
+        programs.btop.package = pkgs.btop-rocm;
+
+        programs.spicetify.enable = true;
+        programs.ghostty.enable = true;
+        programs.firefox.enable = true;
+        programs.slack.enable = true;
+        programs.trilium.enable = true;
+        programs.vscode.enable = true;
+        programs.vesktop.enable = true;
+        programs.bitwarden.enable = true;
+        programs.cemu.enable = true;
+        programs.ryujinx.enable = true;
+        programs.wiiu-downloader.enable = true;
+        programs.element.enable = true;
+        programs.newsflash.enable = true;
+        programs.gemini-cli.enable = true;
+        programs.onlyoffice.enable = true;
+        programs.prismlauncher.enable = true;
+
+        # folder for persistent projects
+        impermanence.directories = [ "git" ];
+
+        # battery is set to charge to 80% max
+        # sudo framework_tool --charge-limit
+        programs.waybar.settings.mainBar.battery.full-at = 80;
+
+        # disable main screen when lid closed
+        wayland.windowManager.hyprland.settings = {
+          bindl = [
+            ",switch:on:Lid Switch,exec,hyprctl keyword monitor 'desc:BOE,disabled'"
+            ",switch:off:Lid Switch,exec,hyprctl keyword monitor 'desc:BOE,preferred,auto,1.566667'"
+          ];
+        };
+
+        wayland.windowManager.hyprland.enable = true;
+      }
+    );
   };
-
-  # amd gpu
-  programs.btop.package = pkgs.btop-rocm;
-
-  programs.spicetify.enable = true;
-  programs.ghostty.enable = true;
-  programs.firefox.enable = true;
-  programs.slack.enable = true;
-  programs.trilium.enable = true;
-  programs.vscode.enable = true;
-  programs.vesktop.enable = true;
-  programs.bitwarden.enable = true;
-  programs.cemu.enable = true;
-  programs.ryujinx.enable = true;
-  programs.wiiu-downloader.enable = true;
-  programs.element.enable = true;
-  programs.newsflash.enable = true;
-  programs.gemini-cli.enable = true;
-  programs.onlyoffice.enable = true;
-  programs.prismlauncher.enable = true;
-
-  # folder for persistent projects
-  impermanence.directories = [ "git" ];
-
-  # battery is set to charge to 80% max
-  # sudo framework_tool --charge-limit
-  programs.waybar.settings.mainBar.battery.full-at = 80;
-
-  # disable main screen when lid closed
-  wayland.windowManager.hyprland.settings = {
-    bindl = [
-      ",switch:on:Lid Switch,exec,hyprctl keyword monitor 'desc:BOE,disabled'"
-      ",switch:off:Lid Switch,exec,hyprctl keyword monitor 'desc:BOE,preferred,auto,1.566667'"
-    ];
-  };
-
-  wayland.windowManager.hyprland.enable = true;
 }
