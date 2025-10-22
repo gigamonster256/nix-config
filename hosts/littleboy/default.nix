a32f2722970f790d4c1413e8c1834308307b16b5
Caleb Norton <n0603919@outlook.com>
2025-10-22 18:35:20 -0500

    littleboy to unify

--- hosts/littleboy/default.nix
+++ hosts/littleboy/default.nix
@@ -1,109 +1,103 @@
-{ inputs, lib, ... }:
+{ inputs, config, ... }:
 {
-  configurations.nixos.littleboy =
-    {
-      modulesPath,
-      pkgs,
-      config,
-      ...
-    }:
-    {
-      # not 100% sure if this is needed
-      imports = [
-        (modulesPath + "/installer/scan/not-detected.nix")
-      ];
+  unify.hosts.nixos.littleboy = {
+    modules = with config.unify.modules; [
+      facter
+      disko
+      secure-boot
+      impermanence
+      wireless
+      desktop
+      style
+    ];
 
-      programs.steam.enable = true;
-
-      # boot config
-      boot = {
-        # tpm2 luks unlock
-        initrd.systemd = {
-          enable = true;
-          emergencyAccess = "$6$5fV/nNXqEFrDtYz7$5.lFDJ3nHnP1Bx9dlEZvZTG2XSO1GFaBb0CV4wT5grM9GrGxGEFVa114shWqlcVu/00WLQWWZiNpAReUb2O4s1";
-        };
-        # secure boot
-        lanzaboote = {
-          enable = true;
-          pkiBundle = "/var/lib/sbctl";
-        };
-        loader = {
-          timeout = 0;
-          systemd-boot.enable = lib.mkForce false; # use lanzaboote
-          efi.canTouchEfiVariables = true;
-        };
-        # binfmt.emulatedSystems = ["aarch64-linux"];
+    users = {
+      caleb = {
+        # Hmm, not sure if I like this pattern - perhaps auto import unify home modules included un the
+        # system modules above?
+        modules = with config.unify.modules; [
+          style
+        ];
       };
+    };
 
-      # extra security https://oddlama.org/blog/bypassing-disk-encryption-with-tpm2-unlock
-      # systemIdentity = {
-      #   enable = true;
-      #   # if this changes, 'systemctl disable check-pcrs; systemctl default' in emergency shell to skip check
-      #   pcr15 = "f3c1ccf9ce465c88851005656454218cccbf4288338a398e6dec035548ceada8";
-      # };
+    nixos =
+      {
+        modulesPath,
+        pkgs,
+        config,
+        lib,
+        ...
+      }:
+      {
+        imports = [
+          # home manager: TODO: remove this
+          (
+            { config, ... }:
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                # TODO: get rid of this
+                extraSpecialArgs = {
+                  systemConfig = config;
+                };
+                sharedModules = [
+                  inputs.nix-index-database.homeModules.nix-index
+                ];
+              };
+            }
+          )
+        ];
+        boot = {
+          initrd.systemd.emergencyAccess = "$6$5fV/nNXqEFrDtYz7$5.lFDJ3nHnP1Bx9dlEZvZTG2XSO1GFaBb0CV4wT5grM9GrGxGEFVa114shWqlcVu/00WLQWWZiNpAReUb2O4s1";
+          # binfmt.emulatedSystems = ["aarch64-linux"];
+        };
+        systemIdentity.pcr15 = "f3c1ccf9ce465c88851005656454218cccbf4288338a398e6dec035548ceada8";
 
-      # impermanence
-      # impermanence = {
-      #   enable = true;
-      #   btrfsWipe.enable = true;
-      # };
+        # littleboy cant do WPA3
+        networking.wireless.fallbackToWPA2 = true;
 
-      # wireless (wpa_supplicant)
-      # TODO: use networkmanager
-      networking.wireless.enable = true;
-      # littleboy cant do WPA3
-      networking.wireless.fallbackToWPA2 = true;
+        # printing
+        # services.printing.enable = true;
 
-      # time zone
-      time.timeZone = "America/Chicago";
+        environment.systemPackages = with pkgs; [
+          vim
+          git
+          catppuccin-sddm
+        ];
 
-      # printing
-      # services.printing.enable = true;
+        programs.zsh.enable = true;
 
-      environment.systemPackages = with pkgs; [
-        vim
-        git
-        catppuccin-sddm
-      ];
+        sops.secrets.caleb-password = {
+          neededForUsers = true;
+          sopsFile = ./secrets.yaml;
+        };
 
-      # hyprland
-      programs.hyprland.enable = true;
-      programs.hyprlock.enable = true;
-
-      programs.zsh.enable = true;
-
-      # hardware
-      facter.reportPath = ./facter.json;
-
-      sops.secrets.caleb-password = {
-        neededForUsers = true;
-        sopsFile = ./secrets.yaml;
-      };
-
-      users = {
-        mutableUsers = false;
         users = {
-          # Replace with your username
-          caleb = {
-            hashedPasswordFile = config.sops.secrets.caleb-password.path;
-            isNormalUser = true;
-            openssh.authorizedKeys.keys = [
-              # Add your SSH public key(s) here, if you plan on using SSH to connect
-            ];
-            # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-            extraGroups = [ "wheel" ];
-            shell = pkgs.zsh;
+          mutableUsers = false;
+          users = {
+            # Replace with your username
+            caleb = {
+              hashedPasswordFile = config.sops.secrets.caleb-password.path;
+              isNormalUser = true;
+              openssh.authorizedKeys.keys = [
+                # Add your SSH public key(s) here, if you plan on using SSH to connect
+              ];
+              # Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+              extraGroups = [ "wheel" ];
+              shell = pkgs.zsh;
+            };
           };
         };
+
+        # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+        system.stateVersion = "24.11";
+
+        # router = {
+        #   enable = true;
+        #   wanInterface = "wlo1";
+        #   lanInterface = "enp0s21f0u1u2";
+        # };
       };
-
-      # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-      system.stateVersion = "24.11";
-
-      # router = {
-      #   enable = true;
-      #   wanInterface = "wlo1";
-      #   lanInterface = "enp0s21f0u1u2";
-      # };
-    };
+  };
 }
