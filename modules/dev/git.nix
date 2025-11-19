e794897a631e04dd814451f62eb0429edead17ac
Caleb Norton <n0603919@outlook.com>
2025-11-19 13:49:10 -0600

    try out difftastic

--- modules/dev/git.nix
+++ modules/dev/git.nix
@@ -1,18 +1,21 @@
-{ config, lib, ... }:
+{ config, ... }:
 {
-  unify.modules.dev.home = {
-    programs.git = {
-      enable = lib.mkDefault true;
-      settings = {
-        user = {
-          name = lib.mkDefault config.meta.owner.name;
-          email = lib.mkDefault config.meta.owner.email;
+  unify.modules.dev.home =
+    { lib, pkgs, ... }:
+    {
+      programs.git = {
+        enable = lib.mkDefault true;
+        settings = {
+          user = {
+            name = lib.mkDefault config.meta.owner.name;
+            email = lib.mkDefault config.meta.owner.email;
+          };
+          alias = {
+            exec = lib.mkDefault "!exec ";
+          };
+          init.defaultBranch = "master";
+          diff.external = lib.mkDefault (lib.getExe pkgs.difftastic);
         };
-        alias = {
-          exec = lib.mkDefault "!exec ";
-        };
-        init.defaultBranch = "master";
       };
     };
-  };
 }
--- modules/dev/jujutsu.nix
+++ modules/dev/jujutsu.nix
@@ -1,6 +1,11 @@
 {
   unify.modules.dev.home =
-    { lib, config, ... }:
+    {
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
     {
       programs.jujutsu.enable = lib.mkDefault true;
       programs.jujutsu.settings = {
@@ -10,9 +15,15 @@
         };
         ui = {
           merge-editor = lib.mkDefault ":builtin";
-          diff-editor = lib.mkDefault ":builtin";
           default-command = lib.mkDefault [ "log" ];
           pager = lib.mkDefault ":builtin";
+          diff-editor = lib.mkDefault ":builtin";
+          diff-formatter = [
+            (lib.getExe pkgs.difftastic)
+            "--color=always"
+            "$left"
+            "$right"
+          ];
         };
         signing = {
           behavior = "keep";
