6112581cceb6f1c74fa7d04a25be8c8add86c54d
Caleb Norton <n0603919@outlook.com>
2025-11-25 10:26:29 -0600

    add dev packages to chnorton home profile

--- modules/standalone-home.nix
+++ modules/standalone-home.nix
@@ -1,8 +1,12 @@
+{ config, ... }:
 {
   configurations.home = {
-    # just my global config for linux
+    # just my global config plus dev for linux
     chnorton = {
       system = "x86_64-linux";
+      module = {
+        imports = [ config.unify.modules.dev.home ];
+      };
     };
   };
 }
