4b182f5b2784faccfd7948e014f5cea7b4776b5b
Caleb Norton <n0603919@outlook.com>
2025-12-07 18:00:54 -0600

    add work printer

--- modules/desktop/printing.nix
+++ modules/desktop/printing.nix
@@ -1,24 +1,53 @@
 {
-  unify.modules.desktop.nixos =
-    { lib, pkgs, ... }:
-    {
-      services.printing = {
-        enable = lib.mkDefault true;
-        drivers = [
+  unify.modules = {
+    desktop.nixos =
+      { lib, pkgs, ... }:
+      {
+        services.printing = {
+          enable = lib.mkDefault true;
+          drivers = [
             pkgs.cups-brother-hll2315dw
+          ];
+        };
+        hardware.printers = {
+          ensureDefaultPrinter = "home_printer";
+          ensurePrinters = [
+            {
+              name = "home_printer";
+              deviceUri = "lpd://printer.penguin";
+              location = "Living Room";
+              # lpinfo -m
+              model = "brother-HLL2315DW-cups-en.ppd";
+              # lpoptions -p <name> -l
+              ppdOptions = {
+                PageSize = "Letter";
+              };
+            }
+          ];
+        };
+      };
+
+    laptop.nixos =
+      { lib, pkgs, ... }:
+      {
+        services.printing = {
+          drivers = [
+            # hplib is pretty heavy, can it be trimmed down?
+            pkgs.hplip
+          ];
+        };
+        hardware.printers.ensurePrinters = [
+          {
+            name = "work_printer";
+            deviceUri = "lpd://ECEN-WEB052-HPP2055dn.engr.tamu.edu";
+            location = "WEB 052";
+            model = "drv:///hp/hpcups.drv/hp-laserjet_p2055dn-pcl3.ppd";
+            ppdOptions = {
+              PageSize = "Letter";
+              OptionDuplex = "True";
+            };
+          }
         ];
       };
-      hardware.printers.ensurePrinters = [
-        {
-          name = "printer";
-          deviceUri = "lpd://printer.penguin";
-          location = "Living Room";
-          model = "brother-HLL2315DW-cups-en.ppd";
-          # lpoptions -p <name> -l
-          ppdOptions = {
-            PageSize = "Letter";
-          };
-        }
-      ];
-    };
+  };
 }
