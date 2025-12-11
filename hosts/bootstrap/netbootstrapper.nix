d7148a96bae10298ecaeed1daaebb06c2d54187d
Caleb Norton <n0603919@outlook.com>
2025-12-11 01:36:08 -0600

    extract netbootstrapper

---
+++ hosts/bootstrap/netbootstrapper.nix
@@ -0,0 +1,22 @@
+{ inputs, ... }:
+{
+  unify.modules.netbootstrapper.nixos = {
+    services.pixiecore =
+      let
+        bootstrap =
+          (inputs.self.nixosConfigurations.bootstrap.extendModules {
+            modules = [
+              "${inputs.nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
+            ];
+          }).config;
+        inherit (bootstrap.system) build;
+      in
+      {
+        enable = true;
+        openFirewall = true;
+        kernel = "${build.kexecTree}/bzImage";
+        initrd = "${build.kexecTree}/initrd.gz";
+        cmdLine = "init=${build.toplevel}/init ${toString bootstrap.boot.kernelParams}";
+      };
+  };
+}
--- hosts/wyse/DX/default.nix
+++ hosts/wyse/DX/default.nix
@@ -1,27 +1,9 @@
-{ inputs, config, ... }:
+{ config, ... }:
 {
   unify.hosts.nixos.wyse-DX = {
     modules = with config.unify.modules; [
       wyse
     ];
-    nixos = {
-      services.pixiecore =
-        let
-          bootstrap =
-            (inputs.self.nixosConfigurations.bootstrap.extendModules {
-              modules = [
-                "${inputs.nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
-              ];
-            }).config;
-          inherit (bootstrap.system) build;
-        in
-        {
-          enable = true;
-          openFirewall = true;
-          kernel = "${build.kexecTree}/bzImage";
-          initrd = "${build.kexecTree}/initrd.gz";
-          cmdLine = "init=${build.toplevel}/init ${toString bootstrap.boot.kernelParams}";
-        };
-    };
+    nixos = { };
   };
 }
