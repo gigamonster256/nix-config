e6aa21f6a071d44b7e31e5a97c8bce8f624fe182
Caleb Norton <n0603919@outlook.com>
2025-12-12 10:43:36 -0600

    dont be so stingy about tts

--- modules/desktop/speechd.nix
+++ modules/desktop/speechd.nix
@@ -1,13 +1,31 @@
 {
-  nixpkgs.overlays = [
-    (_final: prev: {
-      firefox = prev.firefox.override {
-        cfg = {
-          speechSynthesisSupport = false;
-        };
-      };
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (_final: prev: {
+  #     firefox = prev.firefox.override {
+  #       cfg = {
+  #         speechSynthesisSupport = false;
+  #       };
+  #     };
+  #     kdePackages = prev.kdePackages.overrideScope (
+  #       _kfinal: kprev: {
+  #         okular = kprev.okular.overrideAttrs (oldAttrs: {
+  #           # remove qtspeech from buildInputs and tell CMake to not require it
+  #           buildInputs = lib.filter (dep: dep.pname != "qtspeech") oldAttrs.buildInputs;
+  #           cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+  #             "-DFORCE_NOT_REQUIRED_DEPENDENCIES=Qt6TextToSpeech"
+  #           ];
+  #         });
+  #         ktextwidgets = kprev.ktextwidgets.overrideAttrs (oldAttrs: {
+  #           # remove qtspeech from buildInputs and disable text to speech
+  #           buildInputs = lib.filter (dep: dep.pname != "qtspeech") oldAttrs.buildInputs;
+  #           cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+  #             "-DWITH_TEXT_TO_SPEECH=OFF"
+  #           ];
+  #         });
+  #       }
+  #     );
+  #   })
+  # ];
 
   unify.nixos =
     { lib, ... }:
