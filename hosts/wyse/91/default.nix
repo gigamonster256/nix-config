1e334762d578550c57efe0286545d78d85787035
Caleb Norton <n0603919@outlook.com>
2025-12-15 17:41:08 -0600

    uptime kuma

--- hosts/wyse/91/default.nix
+++ hosts/wyse/91/default.nix
@@ -4,6 +4,22 @@
     modules = with config.unify.modules; [
       wyse
     ];
-    nixos = { };
+    nixos = {
+      services.uptime-kuma.enable = true;
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+      services.nginx = {
+        enable = true;
+        virtualHosts."uptime.nortonweb.org" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3001";
+          };
+        };
+      };
+    };
   };
 }
