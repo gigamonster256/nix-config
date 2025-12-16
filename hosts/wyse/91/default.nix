474b5710271a86a375b43baea999b36bdab1aa56
Caleb Norton <n0603919@outlook.com>
2025-12-16 13:38:37 -0600

    uptime-kuma to module

--- hosts/wyse/91/default.nix
+++ hosts/wyse/91/default.nix
@@ -3,23 +3,9 @@
   unify.hosts.nixos.wyse-91 = {
     modules = with config.unify.modules; [
       wyse
+      uptime-kuma
+      backup
     ];
-    nixos = {
-      services.uptime-kuma.enable = true;
-      networking.firewall.allowedTCPPorts = [
-        80
-        443
-      ];
-      services.nginx = {
-        enable = true;
-        virtualHosts."uptime.nortonweb.org" = {
-          enableACME = true;
-          forceSSL = true;
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:3001";
-          };
-        };
-      };
-    };
+    nixos = { };
   };
 }
---
+++ modules/services/uptime-kuma.nix
@@ -0,0 +1,49 @@
+{
+  unify.modules = {
+    uptime-kuma.nixos = {
+      services.uptime-kuma.enable = true;
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+      services.nginx = {
+        enable = true;
+        virtualHosts."uptime.nortonweb.org" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3001";
+          };
+        };
+      };
+    };
+
+    backup.nixos =
+      {
+        lib,
+        pkgs,
+        config,
+        ...
+      }:
+      lib.mkIf config.services.uptime-kuma.enable {
+        # FIXME: unify the backup directories based on a nixos option
+        systemd.services.backup-uptime-kuma = {
+          description = "Backup Uptime Kuma data";
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${lib.getExe pkgs.rsync} -a --delete /var/lib/uptime-kuma/ /mnt/backup/uptime-kuma/";
+          };
+          requires = [ "mnt-backup.mount" ];
+          after = [ "mnt-backup.mount" ];
+        };
+        systemd.timers.backup-uptime-kuma = {
+          description = "Backup of Uptime Kuma data every 8 hours";
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = "*-*-* 00/8:00:00";
+            Persistent = true;
+          };
+        };
+      };
+  };
+}
