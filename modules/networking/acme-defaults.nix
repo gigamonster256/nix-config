c375e38881d9e7d5306071445e7866236b1cdf6d
Caleb Norton <n0603919@outlook.com>
2025-12-20 17:52:43 -0600

    better acme renewal for self-hosted CA

--- modules/networking/acme-defaults.nix
+++ modules/networking/acme-defaults.nix
@@ -1,13 +1,27 @@
 {
-  unify.nixos = {
-    security.acme = {
-      acceptTerms = true;
-      defaults = {
-        email = "admin@nortonweb.org";
-        server = "https://certs.nortonweb.org/acme/acme/directory";
-        webroot = "/var/lib/acme/acme-challenge";
-        renewInterval = "*-*-* 00/12:00:00";
+  unify.nixos =
+    { config, lib, ... }:
+    let
+      # derive acme-renew-* names from defined ACME certs
+      acmeRenewNames = map (n: "acme-renew-" + n) (builtins.attrNames config.security.acme.certs);
+    in
+    {
+      security.acme = {
+        acceptTerms = true;
+        defaults = {
+          email = "admin@nortonweb.org";
+          server = "https://certs.nortonweb.org/acme/acme/directory";
+          webroot = "/var/lib/acme/acme-challenge";
+          # step ca defaults to 24 hr certs, so we renew more frequently
+          renewInterval = "*-*-* 00/12:00:00";
+          # however, the default RandomizedDelaySec is 24 hours, which doesnt work,
+          # do we need to make all acme timers have a smaller randomized delay
+        };
       };
+
+      # override RandomizedDelaySec for all acme-renew-* timers
+      systemd.timers = lib.genAttrs acmeRenewNames (_: {
+        timerConfig.RandomizedDelaySec = lib.mkForce "1h";
+      });
     };
-  };
 }
