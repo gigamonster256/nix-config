265990967db04f78813ab934ee05172e3d847fdb
Caleb Norton <n0603919@outlook.com>
2026-01-19 01:46:06 -0600

    opencode dev with bun version relaxed

--- modules/dev/ai-cli.nix
+++ modules/dev/ai-cli.nix
@@ -1,21 +1,22 @@
-{ inputs, moduleWithSystem, ... }:
+{ inputs, ... }:
 {
-  # TODO: use an overlay rather than moduleWithSystem?
-  unify.modules.dev.home = moduleWithSystem (
-    { system, ... }:
+  nixpkgs.overlays = [
+    # bring in opencode dev and apply nixpkgs patches (bun version relaxation, etc...)
+    (final: prev: {
+      opencode =
+        inputs.opencode.packages.${final.stdenv.hostPlatform.system}.opencode.overrideAttrs
+          (old: {
+            patches = (old.patches or [ ]) ++ prev.opencode.patches;
+          });
+    })
+  ];
+
+  unify.modules.dev.home =
     { lib, ... }:
     {
-      # home.packages = [
-      #   # lets try out desktop version
-      #   inputs.opencode.packages.${system}.desktop
-      # ];
-      programs.opencode = {
-        enable = lib.mkDefault true;
-        package = inputs.opencode.packages.${system}.desktop; # desktop version inclides cli as "opencode-cli"
-      };
+      programs.opencode.enable = lib.mkDefault true;
       # programs.gemini-cli.enable = lib.mkDefault true;
-    }
-  );
+    };
 
   persistence.programs.homeManager = {
     opencode = {
