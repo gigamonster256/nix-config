284478bf07f9ba4ab4fa9545406cfd1c2c3113a5
Caleb Norton <n0603919@outlook.com>
2026-01-24 19:52:03 -0600

    mess with removing x11 deps

--- pkgs/zig-tetris.nix
+++ pkgs/zig-tetris.nix
@@ -4,10 +4,86 @@
       lib,
       stdenv,
       fetchFromGitHub,
+      fetchFromGitLab,
       zig,
       glfw,
+      libdecor,
       libepoxy,
+      libGL,
+      libX11,
     }:
+    let
+      libdecorPatched = libdecor.overrideAttrs (
+        finalAttrs: prevAttrs: {
+          version = "0.2.5";
+          src = fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "libdecor";
+            repo = "libdecor";
+            tag = finalAttrs.version;
+            hash = "sha256-sUktv/k+4IdJ55uH3F6z8XqaAOTic6miuyZ9U+NhtQQ=";
+          };
+          patches = (prevAttrs.patches or [ ]) ++ [
+            ./libdecor.patch
+          ];
+        }
+      );
+      # window keeps shrinking
+      # https://github.com/hyprwm/Hyprland/discussions/12200
+      # https://gitlab.freedesktop.org/libdecor/libdecor/-/issues/80
+      # even with libdecor 0.2.5, the size is still wrong
+      # https://github.com/hyprwm/Hyprland/discussions/12200#discussioncomment-15133605
+      # https://gitlab.freedesktop.org/libdecor/libdecor/-/issues/80#note_3221028
+      # https://github.com/glfw/glfw/issues/2789
+      glfwPatched =
+        (glfw.override {
+          libdecor = libdecorPatched;
+        }).overrideAttrs
+          (prevAttrs: {
+            patches = (prevAttrs.patches or [ ]) ++ [
+              ./glfw.patch
+            ];
+          });
+
+      x11Support = false;
+      _libepoxyNoX11 =
+        (libepoxy.override {
+          inherit x11Support;
+        }).overrideAttrs
+          (
+            finalAttrs: _prevAttrs: {
+              # x11Support is too integrated into macos vs linux
+              propagatedBuildInputs =
+                lib.optionals (!stdenv.hostPlatform.isDarwin) [
+                  libGL
+                ]
+                ++ lib.optionals x11Support [
+                  libX11
+                ];
+              mesonFlags = [
+                "-Degl=${lib.boolToYesNo (!stdenv.hostPlatform.isDarwin)}"
+                "-Dglx=${lib.boolToYesNo x11Support}"
+                "-Dtests=${lib.boolToString finalAttrs.finalPackage.doCheck}"
+                "-Dx11=${lib.boolToString x11Support}"
+              ];
+              env.NIX_CFLAGS_COMPILE = lib.optionalString (
+                !stdenv.hostPlatform.isDarwin
+              ) ''-DLIBGL_PATH="${lib.getLib libGL}/lib"'';
+            }
+          );
+
+      glfwNoX11 = glfwPatched.overrideAttrs (
+        _finalAttrs: prevAttrs: {
+          cmakeFlags = prevAttrs.cmakeFlags ++ [
+            (lib.cmakeBool "GLFW_BUILD_X11" false)
+          ];
+          # filter "libx*" buildInputs except libxkeyboard
+          buildInputs = lib.filter (
+            p: !(lib.hasPrefix "libx" (lib.getName p) && lib.getName p != "libxkbcommon")
+          ) prevAttrs.buildInputs;
+        }
+      );
+    in
     stdenv.mkDerivation (finalAttrs: {
       pname = "zig-tetris";
       version = "0.1.0";
@@ -20,12 +96,14 @@
       };
 
       nativeBuildInputs = [
-        zig.hook
+        zig
       ];
 
       buildInputs = [
-        glfw
-        libepoxy
+        # glfw
+        glfwNoX11
+        _libepoxyNoX11
+        # libepoxy
       ];
 
       meta = {
