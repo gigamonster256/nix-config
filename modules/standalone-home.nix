106452167e44e871fe5bdd706456ea6484725d92
Caleb Norton <n0603919@outlook.com>
2026-02-05 13:54:59 -0600

    feat: secureish opencode server

--- modules/standalone-home.nix
+++ modules/standalone-home.nix
@@ -1,11 +1,20 @@
-{ config, ... }:
+{ config, ... }@flake:
 {
   configurations.home = {
     # just my global config plus dev for linux
     chnorton = {
       system = "x86_64-linux";
-      module = {
-        imports = [ config.unify.modules.dev.home ];
+      module = {lib, pkgs,config,...}:{
+        imports = [ flake.config.unify.modules.dev.home ];
+        systemd.user.services.opencode = {
+          # TODO: secure further - this is basic-auth over http... bad
+          Service = {
+            # listen on all interfaces for opencode, password is set by OPENCODE_SERVER_PASSWORD
+            ExecStart = lib.mkForce "${lib.getExe config.programs.opencode.package} serve --port 40123 --hostname 0.0.0.0";
+            # FIXME: sops?
+            EnvironmentFile = "/home/chnorton/.config/opencode/env";
+          };
+        };
       };
     };
   };
