2465fc3d8c651a1ea2dbaab5de44fdd197d87165
Caleb Norton <n0603919@outlook.com>
2026-02-05 13:31:56 -0600

    add opencode bg server

--- modules/dev/ai-cli.nix
+++ modules/dev/ai-cli.nix
@@ -8,10 +8,49 @@
   ];
 
   unify.modules.dev.home =
-    { lib, ... }:
+    {
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
     {
       programs.opencode.enable = lib.mkDefault true;
       # programs.gemini-cli.enable = lib.mkDefault true;
+
+      systemd.user.services.opencode = {
+        Unit = {
+          Description = "Opencode AI CLI";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${lib.getExe config.programs.opencode.package} serve --port 40123";
+          Restart = "on-failure";
+        };
+
+        Install = {
+          WantedBy = [ "graphical-session.target" ];
+        };
+      };
+
+      programs.waybar.settings.mainBar = {
+        "custom/opencode" =
+          let
+            # FIXME: this is duplicated from waybar config, should be refactored
+            # IDEA: write a "trayify" app that wraps an app in a system tray icon?
+            icon = symbol: "<span font_desc='Font Awesome 7 Free'>${symbol}</span>";
+          in
+          {
+            format = icon "";
+            interval = "once";
+            on-click = "${lib.getExe' pkgs.xdg-utils "xdg-open"} http://127.0.0.1:40123";
+            tooltip = false;
+          };
+        # TODO: better ordering
+        modules-right = lib.mkBefore [ "custom/opencode" ];
+      };
     };
 
   persistence.programs.homeManager = {
