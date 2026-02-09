0d168e849280fc8c234aa908066eac96d89eb61d
Caleb Norton <n0603919@outlook.com>
2026-02-09 14:23:38 -0600

    n7m-t8r: 0.0.3 -> 0.0.4

--- pkgs/n7m-t8r.nix
+++ pkgs/n7m-t8r.nix
@@ -1,26 +1,28 @@
 {
   packages.n7m-t8r =
     {
+      buildNpmPackage,
       fetchFromGitHub,
-      stdenvNoCC,
+      imagemagick,
     }:
-    stdenvNoCC.mkDerivation (finalAttrs: {
+    buildNpmPackage (finalAttrs: {
       pname = "n7m-t8r";
-      version = "0.0.3";
+      version = "0.0.4";
 
       src = fetchFromGitHub {
         owner = "gigamonster256";
         repo = "n7m-t8r";
         tag = "v${finalAttrs.version}";
-        hash = "sha256-kCtm5BHhAt1mlucIkJeCnGSqXuioZZl713CHXQRSHkE=";
+        hash = "sha256-hCMcWHB2nQCVnf18fCQEq2A4/IgumtfbMrAcIciqaco=";
       };
 
-      dontConfigure = true;
-      dontBuild = true;
+      npmDepsHash = "sha256-PLN0yEBYkBbfMpg9npwNdun36mgWwPVU9WA/j5+VDf0=";
+
+      nativeBuildInputs = [ imagemagick ];
 
       installPhase = ''
-        mkdir -p $out
-        cp index.html $out/
+        mkdir -p $out/
+        cp -r dist/* $out/
       '';
     });
 }
