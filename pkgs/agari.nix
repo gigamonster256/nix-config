f1e15b2265d41a7d134a536e412027b938710b10
Caleb Norton <n0603919@outlook.com>
2026-02-21 22:29:07 -0600

    feat: agari apps

---
+++ pkgs/agari.nix
@@ -0,0 +1,93 @@
+{
+  packages.agari =
+    {
+      fetchFromGitHub,
+      rustPlatform,
+    }:
+    rustPlatform.buildRustPackage (finalAttrs: {
+      pname = "agari";
+      version = "0.19.1";
+
+      src = fetchFromGitHub {
+        owner = "rysb-dev";
+        repo = "agari";
+        tag = "v${finalAttrs.version}";
+        hash = "sha256-lSf/H41ySM8ndE2s6TxcLEEyOZ2HuZvgxoR/9PTWNaU=";
+      };
+
+      cargoHash = "sha256-lUvFcafAXykZfiUAy/w8s6tX0X7tQocG8IJ+skvlRF4=";
+
+      meta = {
+        description = "Riichi Score Calculator";
+        homepage = "https://github.com/rysb-dev/agari";
+        mainProgram = "agari";
+      };
+    });
+
+  packages.agari-wasm =
+    {
+      rustPlatform,
+      agari,
+      rustc,
+      wasm-pack,
+      wasm-bindgen-cli,
+      writableTmpDirAsHomeHook,
+    }:
+    rustPlatform.buildRustPackage (_finalAttrs: {
+      pname = "agari-wasm";
+      inherit (agari) version;
+
+      inherit (agari) src cargoHash;
+
+      nativeBuildInputs = [
+        writableTmpDirAsHomeHook
+        rustc.llvmPackages.lld
+        wasm-pack
+        wasm-bindgen-cli
+      ];
+
+      buildPhase = ''
+        wasm-pack build crates/agari-wasm --target web --mode no-install
+      '';
+
+      installPhase = ''
+        mkdir -p $out/
+        cp -r crates/agari-wasm/pkg/* $out/
+      '';
+    });
+
+  packages.agari-web =
+    {
+      buildNpmPackage,
+      agari,
+      agari-wasm,
+    }:
+    buildNpmPackage {
+      pname = "agari-web";
+      inherit (agari) version;
+
+      inherit (agari) src;
+      sourceRoot = "${agari.src.name}/web";
+
+      npmDepsHash = "sha256-VT1DND2+CK8uuDedcJGKtbeBieAKfK6oedADbqrJF/A=";
+
+      preBuild = ''
+        cp -r ${agari-wasm}/* src/lib/wasm/
+      '';
+
+      installPhase = ''
+        mkdir -p $out/
+        cp -r dist/* $out/
+      '';
+    };
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      apps.agari-web.program = pkgs.writeShellApplication {
+        name = "agari-web";
+        runtimeInputs = [ pkgs.python3 ];
+        text = "python3 -m http.server --directory ${pkgs.agari-web} -b 127.0.0.1 8080";
+      };
+    };
+}
