bea8f30abc4cdee20d8a951492f4d74c2ad364f0
Caleb Norton <n0603919@outlook.com>
2026-02-27 13:08:53 -0600

    feat: better vscode config

--- modules/dev/vscode.nix
+++ modules/dev/vscode.nix
@@ -3,15 +3,33 @@
     "vscode"
   ];
 
-  unify.modules.dev.home = {
-    programs.vscode.enable = true;
-    programs.vscode.profiles.default.userSettings = {
-      "files.autoSave" = "afterDelay";
-      "terminal.integrated.fontFamily" = "Monaspace Neon";
-      "terminal.integrated.suggest.enabled" = false;
-      "terminal.integrated.suggest.inlineSuggestion" = "off";
+  unify.modules.dev.home =
+    { lib, osConfig, ... }:
+    {
+      programs.vscode = {
+        enable = true;
+        argvSettings = {
+          enable-crash-reporter = false;
+          password-store = lib.mkIf (
+            osConfig != null && osConfig.services.gnome.gnome-keyring.enable
+          ) "gnome-libsecret";
+        };
+        profiles.default = {
+          enableUpdateCheck = false;
+          userSettings = {
+            "telemetry.telemetryLevel" = "off";
+            "telemetry.enableTelemetry" = false;
+            "telemetry.enableCrashreporter" = false;
+
+            "files.autoSave" = "afterDelay";
+
+            "terminal.integrated.fontFamily" = "Monaspace Neon";
+            "terminal.integrated.suggest.enabled" = false;
+            "terminal.integrated.suggest.inlineSuggestion" = "off";
+          };
+        };
+      };
     };
-  };
 
   persistence.programs.homeManager = {
     vscode = {
