a39d7745272c99f368a90678327016e096d8fbd8
Caleb Norton <n0603919@outlook.com>
2026-03-03 23:27:48 -0600

    feat: better opencode version info

--- modules/dev/ai-cli.nix
+++ modules/dev/ai-cli.nix
@@ -2,7 +2,23 @@
 {
   nixpkgs.overlays = [
     # bring in opencode dev
-    inputs.opencode.overlays.default
+    (final: prev: {
+      opencode = (inputs.opencode.overlays.default final prev).opencode.overrideAttrs (oldAttrs: {
+        # waiting for https://github.com/anomalyco/opencode/pull/11915
+        version = final.lib.replaceString "-" "+" oldAttrs.version;
+        __intentionallyOverridingVersion = true;
+        patches = (oldAttrs.patches or [ ]) ++ [
+          (final.fetchpatch2 {
+            url = "https://github.com/anomalyco/opencode/commit/b546b174cc2c7065559cd33eea3b48673d569f7f.patch?full_index=1";
+            hash = "sha256-vfRrSXlDXhsLNyTPwTDYJjCB1P0Xrwx7nNxjy9NYAig=";
+          })
+          (final.fetchpatch2 {
+            url = "https://github.com/anomalyco/opencode/commit/a382d844935ea9e8b6d3bd4ef1554c9dedc706d8.patch?full_index=1";
+            hash = "sha256-J3XsbU3qsycubmxpBJBEzy9TMYWPJOc3b003lW9AKSw=";
+          })
+        ];
+      });
+    })
   ];
 
   unify.modules.dev.home =
