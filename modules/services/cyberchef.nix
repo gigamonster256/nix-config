5e9fba0e118e2c93d2e4fbf587a7b872d94ac3e0
Caleb Norton <n0603919@outlook.com>
2026-03-05 13:01:52 -0600

    feat: cyberchef service/app

---
+++ modules/services/cyberchef.nix
@@ -0,0 +1,30 @@
+{
+  unify.modules.cyberchef.nixos =
+    { pkgs, ... }:
+    {
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+      services.nginx = {
+        enable = true;
+        virtualHosts."chef.nortonweb.org" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            root = "${pkgs.cyberchef}/share/cyberchef";
+          };
+        };
+      };
+    };
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      apps.cyberchef.program = pkgs.writeShellApplication {
+        name = "cyberchef";
+        runtimeInputs = [ pkgs.python3 ];
+        text = "python3 -m http.server --directory ${pkgs.cyberchef}/share/cyberchef --bind 127.0.0.1 8080";
+      };
+    };
+}
