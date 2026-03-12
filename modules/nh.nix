ea35038108d1196a838efaec12719e2736441780
Caleb Norton <n0603919@outlook.com>
2026-03-12 14:52:20 -0500

    fix: nh zsh completions

--- modules/nh.nix
+++ modules/nh.nix
@@ -1,5 +1,18 @@
 { lib, config, ... }:
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      nh-unwrapped = prev.nh-unwrapped.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (final.fetchpatch2 {
+            url = "https://patch-diff.githubusercontent.com/raw/nix-community/nh/pull/592.patch?full_index=1";
+            hash = "sha256-McF1XObLlVdW5pQ1LhKz1HLrgteFkW4fKiKm0LgeH4I=";
+          })
+        ];
+      });
+    })
+  ];
+
   unify.home = {
     programs.nh = {
       enable = lib.mkDefault true;
