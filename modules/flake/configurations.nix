e1f0b46d4b127596a732b17acf12952d870766ca
Caleb Norton <n0603919@outlook.com>
2026-03-14 13:11:00 -0500

    chore: nix fmt

--- modules/flake/configurations.nix
+++ modules/flake/configurations.nix
@@ -38,94 +38,93 @@ let
         };
       };
 
-      config.flake =
-        {
-          nixosConfigurations = lib.flip lib.mapAttrs cfg.nixos (
-            name: module:
-            inputs.nixpkgs.lib.nixosSystem {
-              specialArgs = { inherit name; }; # kinda hacky but makes disko module not infinitely recurse
-              modules = [
-                # the host configuration
-                module
-                {
-                  networking.hostName = lib.mkDefault name;
-                }
-                inputs.home-manager.nixosModules.home-manager
-                # internal modules
-                inputs.self.modules.nixos.default
-                # home manager
-                {
-                  home-manager = {
-                    useGlobalPkgs = true;
-                    sharedModules = lib.singleton inputs.self.modules.homeManager.default;
-                  };
-                }
-              ];
-            }
-          );
+      config.flake = {
+        nixosConfigurations = lib.flip lib.mapAttrs cfg.nixos (
+          name: module:
+          inputs.nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit name; }; # kinda hacky but makes disko module not infinitely recurse
+            modules = [
+              # the host configuration
+              module
+              {
+                networking.hostName = lib.mkDefault name;
+              }
+              inputs.home-manager.nixosModules.home-manager
+              # internal modules
+              inputs.self.modules.nixos.default
+              # home manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  sharedModules = lib.singleton inputs.self.modules.homeManager.default;
+                };
+              }
+            ];
+          }
+        );
 
-          darwinConfigurations = lib.flip lib.mapAttrs cfg.darwin (
-            name: module:
-            inputs.nix-darwin.lib.darwinSystem {
-              modules = [
-                # the host configuration
-                module
-                {
-                  networking.hostName = lib.mkDefault name;
-                }
-                inputs.home-manager.darwinModules.home-manager
-                # internal modules
-                inputs.self.modules.darwin.base
-                inputs.self.modules.darwin.style
-                # home manager
-                {
-                  home-manager = {
-                    useGlobalPkgs = true;
-                    sharedModules = lib.singleton inputs.self.modules.homeManager.default;
-                  };
-                }
-              ];
-            }
-          );
+        darwinConfigurations = lib.flip lib.mapAttrs cfg.darwin (
+          name: module:
+          inputs.nix-darwin.lib.darwinSystem {
+            modules = [
+              # the host configuration
+              module
+              {
+                networking.hostName = lib.mkDefault name;
+              }
+              inputs.home-manager.darwinModules.home-manager
+              # internal modules
+              inputs.self.modules.darwin.base
+              inputs.self.modules.darwin.style
+              # home manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  sharedModules = lib.singleton inputs.self.modules.homeManager.default;
+                };
+              }
+            ];
+          }
+        );
 
-          homeConfigurations = lib.flip lib.mapAttrs cfg.home (
-            name:
-            { system, module }:
-            inputs.home-manager.lib.homeManagerConfiguration (
-              withSystem system (
-                { pkgs, ... }:
-                {
-                  inherit pkgs;
-                  modules = [
-                    module
-                    {
-                      # TODO: get rid of @ in nane - ex. caleb@littleboy becomes caleb
-                      home.username = lib.mkDefault name;
-                      # TODO: - adapt for non-linux?
-                      home.homeDirectory = lib.mkDefault ("/home/" + name);
-                    }
-                    # standalone specific
-                    inputs.stylix.homeModules.stylix
-                    inputs.self.modules.homeManager.standalone
-                    inputs.self.modules.homeManager.style
-                  ]
-                  ++ lib.singleton inputs.self.modules.homeManager.default;
-                }
-              )
+        homeConfigurations = lib.flip lib.mapAttrs cfg.home (
+          name:
+          { system, module }:
+          inputs.home-manager.lib.homeManagerConfiguration (
+            withSystem system (
+              { pkgs, ... }:
+              {
+                inherit pkgs;
+                modules = [
+                  module
+                  {
+                    # TODO: get rid of @ in nane - ex. caleb@littleboy becomes caleb
+                    home.username = lib.mkDefault name;
+                    # TODO: - adapt for non-linux?
+                    home.homeDirectory = lib.mkDefault ("/home/" + name);
+                  }
+                  # standalone specific
+                  inputs.stylix.homeModules.stylix
+                  inputs.self.modules.homeManager.standalone
+                  inputs.self.modules.homeManager.style
+                ]
+                ++ lib.singleton inputs.self.modules.homeManager.default;
+              }
             )
-          );
+          )
+        );
 
-          # checks = lib.pipe config.flake.nixosConfigurations [
-          #   (lib.mapAttrsToList (
-          #     name: nixos: {
-          #       ${nixos.config.nixpkgs.hostPlatform.system} = {
-          #         "configurations/nixos/${name}" = nixos.config.system.build.toplevel;
-          #       };
-          #     }
-          #   ))
-          #   lib.mkMerge
-          # ];
-        };
+        # checks = lib.pipe config.flake.nixosConfigurations [
+        #   (lib.mapAttrsToList (
+        #     name: nixos: {
+        #       ${nixos.config.nixpkgs.hostPlatform.system} = {
+        #         "configurations/nixos/${name}" = nixos.config.system.build.toplevel;
+        #       };
+        #     }
+        #   ))
+        #   lib.mkMerge
+        # ];
+      };
     };
 in
 {
