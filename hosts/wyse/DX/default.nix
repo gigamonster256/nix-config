1190052b924e0367a51affef5b9a9c924f8a4509
Caleb Norton <n0603919@outlook.com>
2026-05-24 12:08:43 -0500

    feat: calibre-web

--- hosts/wyse/DX/default.nix
+++ hosts/wyse/DX/default.nix
@@ -1,11 +1,26 @@
 { self, ... }:
 {
-  configurations.nixos.wyse-DX = {
-    imports = with self.modules.nixos; [
-      wyse
-      technitium-dns
-      backup
-    ];
-    services.technitium-dns-server.hostName = "ns1.nortonweb.org";
-  };
+  configurations.nixos.wyse-DX =
+    { pkgs, config, ... }:
+    {
+      imports = with self.modules.nixos; [
+        wyse
+        technitium-dns
+        backup
+      ];
+      services.technitium-dns-server.hostName = "ns1.nortonweb.org";
+      # TODO: moduleify
+      # not using selfhosted CA/ACME since root certs on kobo are embedded and
+      # adding my own root seems hard so just let traefik handle it
+      services.calibre-web.enable = true;
+      services.calibre-web.package = pkgs.calibre-web.overridePythonAttrs (prev: {
+        # see nixpkgs drv for more optional deps
+        dependencies = prev.dependencies ++ prev.optional-dependencies.kobo;
+      });
+      services.calibre-web.options.calibreLibrary = "/var/lib/calibre-web/library";
+      services.calibre-web.options.enableKepubify = true;
+      # docker stack (traefik) is ipv4 only and listening on :: only listens on ipv6 for clibre-web
+      services.calibre-web.listen.ip = "0.0.0.0";
+      services.calibre-web.openFirewall = true;
+    };
 }
